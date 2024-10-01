f722633f9c0f330b40a85e331b4a57d8d825eabc
Aiden Rogers <contact@aidenr.dev>
2024-10-02 08:04:23 +1000

    feat: Enhance authentication management by integrating authentication state provider and adding necessary package references

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Components.Web;
+
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using MyPortfolio;
 using MudBlazor.Services;
 
@@ -8,7 +11,9 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddMudServices();
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddApiAuthorization();
 
 await builder.Build().RunAsync();
