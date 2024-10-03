860856ee1ab5e4ddaa1c77ec344a0a679a179be2
Aiden Rogers <contact@aidenr.dev>
2024-10-03 19:19:50 +1000

    feat: Refactor authentication state retrieval and add route permissions in staticwebapp.config.json

--- Authentication/CustomAuthStateProvider.cs
+++ Authentication/CustomAuthStateProvider.cs
@@ -1,68 +1,25 @@
 using Microsoft.AspNetCore.Components.Authorization;
-using System.Net.Http;
-using System.Net.Http.Json;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
 public class CustomAuthStateProvider : AuthenticationStateProvider
 {
-    private readonly HttpClient _httpClient;
-
-    public CustomAuthStateProvider(HttpClient httpClient)
+    public override Task<AuthenticationState> GetAuthenticationStateAsync()
     {
-        _httpClient = httpClient;
+        // Implement logic to get the authentication state
+        var identity = new ClaimsIdentity();
+        var user = new ClaimsPrincipal(identity);
+        return Task.FromResult(new AuthenticationState(user));
     }
 
-    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    public void NotifyAuthenticationStateChanged()
     {
-        Console.WriteLine("Getting authentication state...");
-
-        // Replace with your logic to get the token
-        var token = await GetTokenAsync();
-
-        if (string.IsNullOrEmpty(token))
+        var identity = new ClaimsIdentity(new[]
         {
-            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
-        }
+            new Claim(ClaimTypes.Name, "GitHubUser")
+        }, "github");
 
-        var request = new HttpRequestMessage(HttpMethod.Get, "https://api.github.com/user");
-        request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
-        request.Headers.UserAgent.ParseAdd("request");
-
-        Console.WriteLine("Requesting user info...");
-
-        var response = await _httpClient.SendAsync(request);
-
-        Console.WriteLine(response.StatusCode);
-
-        if (response.IsSuccessStatusCode)
-        {
-            var userInfo = await response.Content.ReadFromJsonAsync<UserInfo>();
-
-            var identity = new ClaimsIdentity(new[]
-            {
-                new Claim(ClaimTypes.Name, userInfo.Login),
-                new Claim(ClaimTypes.NameIdentifier, userInfo.Id.ToString()),
-                new Claim("urn:github:avatar", userInfo.AvatarUrl)
-            }, "GitHub");
-
-            var user = new ClaimsPrincipal(identity);
-
-            return new AuthenticationState(user);
-        }
-
-        return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()));
+        var user = new ClaimsPrincipal(identity);
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
     }
-
-    private async Task<string> GetTokenAsync()
-    {
-        return await Task.FromResult<string>(null);
-    }
-}
-
-public class UserInfo
-{
-    public string Login { get; set; }
-    public int Id { get; set; }
-    public string AvatarUrl { get; set; }
 }
