9f794619247c7eb8e41c81529301ce257d32881d
Aiden Rogers <contact@aidenr.dev>
2024-10-07 16:17:28 +1100

    Refactor ExperienceCards component to use MudContainer and update icon styling

--- Program.cs
+++ Program.cs
@@ -2,13 +2,17 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using MyPortfolio;
+using MyPortfolio.Themes;
 using Microsoft.Extensions.Configuration;
+using MudBlazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddMudServices();
+builder.Services.AddSingleton<MudTheme, CustomTheme>();
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
---
+++ Themes/CustomTheme.cs
@@ -0,0 +1,47 @@
+using MudBlazor;
+
+namespace MyPortfolio.Themes
+{
+    public class CustomTheme : MudTheme
+    {
+        public CustomTheme()
+        {
+            Typography = new Typography()
+            {
+                Default = new Default()
+                {
+                    FontFamily = new[] { "Roboto" },
+                },
+                Button = new Button()
+                {
+                    FontFamily = new[] { "Jetbrains Mono" },
+                },
+                H1 = new H1() { FontSize = "2.5rem" },
+                H2 = new H2() { FontSize = "2rem" },
+                H3 = new H3() { FontSize = "1.75rem" },
+                H4 = new H4() { FontSize = "1.5rem" },
+                Body2 = new Body2() { FontSize = "small" },
+            };
+
+            PaletteLight = new PaletteLight()
+            {
+                Background = "#f5f5f5",
+                Primary = "#8B0000",
+                Secondary = "#8B0000",
+                TextPrimary = "#333",
+                AppbarBackground = "#f5f5f5",
+                Tertiary = "rgba(174,145,164,0.3)"
+            };
+
+            PaletteDark = new PaletteDark()
+            {
+                Background = "#1A1A27",
+                AppbarBackground = "#1A1A27",
+                Primary = "#FFC701",
+                TextPrimary = "#CBD6ED",
+                Secondary = "#FFC701",
+                Tertiary = "rgba(45,40,38,0.3)"
+            };
+        }
+    }
+}
