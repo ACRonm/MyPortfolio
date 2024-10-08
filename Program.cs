8fdeff0b79b5f81a3268bf4d42db3decbd74aeb2
Aiden Rogers <contact@aidenr.dev>
2024-10-08 21:20:48 +1100

    Refactor ExperienceCards component and MainLayout.razor

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using MyPortfolio;
 using MyPortfolio.Themes;
+using Microsoft.Extensions.Configuration;
 using MudBlazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -13,7 +14,5 @@ builder.Services.AddMudServices();
 builder.Services.AddSingleton<MudTheme, CustomTheme>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-builder.Services.AddApiAuthorization();
 
 await builder.Build().RunAsync();
