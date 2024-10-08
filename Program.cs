cdfbb7c8bd69d798e445e40ecd7f72e4c886e3df
Aiden Rogers <contact@aidenr.dev>
2024-10-08 19:23:30 +1100

    Refactor MainLayout and MyPortfolio.csproj files

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using MyPortfolio;
 using MyPortfolio.Themes;
-using Microsoft.Extensions.Configuration;
 using MudBlazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -14,5 +13,7 @@ builder.Services.AddMudServices();
 builder.Services.AddSingleton<MudTheme, CustomTheme>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddApiAuthorization();
 
 await builder.Build().RunAsync();
