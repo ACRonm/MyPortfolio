30a509182d8196c8f4ad9dcf8597a45852a51544
Aiden Rogers <contact@aidenr.dev>
2024-10-12 16:36:44 +1100

    Refactor BlogPostTeaser to load markdown content from GitHub API and improve error handling

--- Program.cs
+++ Program.cs
@@ -14,4 +14,13 @@ builder.Services.AddSingleton<MudTheme, CustomTheme>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.Configure<GitHubSettings>(options => builder.Configuration.GetSection("GitHub").Bind(options));
+
+var movieApiKey = builder.Configuration["GITHUB_TOKEN"];
+
 await builder.Build().RunAsync();
+
+public class GitHubSettings
+{
+    public string? Token { get; set; }
+}
