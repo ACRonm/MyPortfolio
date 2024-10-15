830fa2c0dcaa8246706cc3ecc67a4981ca344e2a
Aiden Rogers <contact@aidenr.dev>
2024-10-15 21:15:41 +1100

    Refactor Blog.razor and ContentService: Enhance BlogPostTeaser with Title and streamline content parsing

--- Services/ContentService.cs
+++ Services/ContentService.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Diagnostics;
 using System.Net;
 using System.Net.Http.Json;
+using System.Reflection.Metadata;
 using System.Text.Json;
+using HtmlAgilityPack;
+using Markdig;
 
 
 namespace MyPortfolio.Services
@@ -11,13 +15,13 @@ namespace MyPortfolio.Services
         private readonly IConfiguration Configuration;
         private readonly HttpClient Http = new HttpClient();
         private List<GitHubFile>? GitHubFiles = new List<GitHubFile>();
+        private string errorMessage = string.Empty;
 
 
         public ContentService(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-        private List<string> markdownContent = new List<string>();
 
         public async Task<List<GitHubFile>> LoadContentAsync()
         {
@@ -39,8 +43,12 @@ namespace MyPortfolio.Services
 
             foreach (var file in GitHubFiles)
             {
-
                 var fileResponse = await Http.GetFromJsonAsync<GitHubFile>(file.Url);
+
+                if (!string.IsNullOrEmpty(file.Name))
+                {
+                    file.Name = Path.GetFileNameWithoutExtension(file.Name);
+                }
 
                 if (fileResponse?.Content != null)
                 {
@@ -55,13 +63,67 @@ namespace MyPortfolio.Services
                     file.CommitDate = commitResponse[0].GetProperty("commit").GetProperty("committer").GetProperty("date").GetString();
                     file.Committer = commitResponse[0].GetProperty("commit").GetProperty("committer").GetProperty("name").GetString();
                 }
+
+                file.Content = file.Content != null ? ParseMarkdownToHtml(file.Content) : string.Empty;
+
+                var doc = new HtmlDocument();
+                doc.LoadHtml(file.Content);
+
+                file.ImageUrl = ParseTeaserImage(doc);
+
+                file.Tags = GetTags(doc);
+
             }
             return GitHubFiles;
         }
 
+        public string ParseMarkdownToHtml(string content)
+        {
+            content = content != null ? Markdown.ToHtml(content) : string.Empty;
+
+
+            return content;
+        }
+
+        public string ParseTeaserImage(HtmlDocument content)
+        {
+            var imgNode = content.DocumentNode.SelectSingleNode("//img");
+            string imageUrl = imgNode != null ? imgNode.GetAttributeValue("src", "defaultImage.jpg") : "defaultImage.jpg";
+
+            return imageUrl;
+        }
+
+
+        // TODO: Ensure that tags are correctly removed from the document and returned as a list of strings
+        public List<string> GetTags(HtmlDocument content)
+        {
+            var tags = new List<string>();
+            var tagsNode = content.DocumentNode.SelectSingleNode("//p[text()='tags:']/following-sibling::ul");
+            var tagsParagraph = content.DocumentNode.SelectSingleNode("//p[text()='tags:']");
+
+            
+            if (tagsNode != null)
+            {
+                foreach (var tag in tagsNode.SelectNodes("li"))
+                {
+                    Console.WriteLine(tag.InnerText);
+                    tags.Add(tag.InnerText);
+                    tagsNode.Remove();
+                }
+            }
+            if (tagsParagraph != null)
+            {
+                tagsParagraph.Remove();
+            }
+
+            Console.WriteLine(tags);
+
+            return tags;
+        }
         public class GitHubFile
         {
             public string? Name { get; set; }
+            public string? Title { get; set; }
             public string? Url { get; set; }
             public string? Content { get; set; }
             public string? CommitDate { get; set; }
