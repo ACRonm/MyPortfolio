95dbab6e1eba58e0fd9fd30625635cdcd11f6b80
ACRnm <contact@aidenr.dev>
2024-12-01 20:11:44 +1100

    Add sharing functionality to BlogpostTeaser component and implement share script

---
+++ wwwroot/javascript/share.js
@@ -0,0 +1,13 @@
+function shareContent(title, text, url) {
+    if (navigator.share) {
+        navigator.share({
+            title: title,
+            text: text,
+            url: url,
+        })
+        .then(() => console.log('Successful share'))
+        .catch((error) => console.log('Error sharing', error));
+    } else {
+        alert('Share not supported on this browser.');
+    }
+}
